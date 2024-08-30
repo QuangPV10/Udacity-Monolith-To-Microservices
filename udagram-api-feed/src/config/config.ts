0d7d1f1da3f404819c7ad8a513316cb8dc834b93
QuangPV10 <phamvanquang.work@gmail.com>
2024-08-30 15:42:08 +0700

    done part II

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,15 +1,14 @@
-require('dotenv').config();
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'postgres',
+  'password': '123456789',
+  'database': 'postgres',
+  'host': 'cdr.c1r6smdcgv7r.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': "monolith-to-microservices",
-  'url': process.env.URL,
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'monolith-to-microservices',
+  'url': 'http://localhost:8080',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'testing',
   },
 };
